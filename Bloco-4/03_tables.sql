e6541457e4af215fb2d37ef86029e46883876fe6
MATHEUS EGON SCHNEIDER <matheus_schneider150@estudante.sesisenai.org.br>
2026-06-24 16:05:32 -0300

    tables-Item_Cotacao

--- Bloco-4/03_tables.sql
+++ Bloco-4/03_tables.sql
@@ -17,3 +17,13 @@ CREATE TABLE Cotacao_Personalizadas (
     FOREIGN KEY (id_oportunidade) REFERENCES Oportunidade_CRM(id_oportunidade),
     FOREIGN KEY (id_pacote) REFERENCES Pacote(id_pacote)
 );
+
+CREATE TABLE Item_Cotacao (
+    id_item_cotacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_cotacao INT NOT NULL,
+    id_modulo INT NOT NULL,
+    valor_aplicado DECIMAL(10, 2) NOT NULL,
+    UNIQUE (id_cotacao, id_modulo),
+    FOREIGN KEY (id_cotacao) REFERENCES Cotacao_Personalizadas(id_cotacao),
+    FOREIGN KEY (id_modulo) REFERENCES Modulos_Pacote(id_modulo)
+);
